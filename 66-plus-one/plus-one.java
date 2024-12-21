9e42e56638ecb20ff7bf87f31f5dc0be706c40f2
Michael Odusami <kyrmikee@gmail.com>
2024-12-20 20:09:26 -0500

    Time: 0 ms (100.00%) | Memory: 42.4 MB (5.29%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,36 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int ldx = digits.length - 1;
-        int last = digits[ldx];
-        int ilast = last + 1;
-        String ilastStr = String.valueOf(ilast);
-        if (ilastStr.charAt(ilastStr.length() - 1) == '0')
+        for (int i = digits.length - 1; i >= 0; i--)
         {
-
-            while (ldx > -1 && digits[ldx] == 9)
+            if (digits[i] + 1 != 10)
             {
-                digits[ldx] = 0;
-                ldx--;
+                digits[i] += 1;
+                return digits;
             }
-
-            if (ldx == -1)
-            {
-                int length = digits.length + 1;
-                int[] newArr = new int[length];
-                newArr[0] = 1;
-                return newArr;
-            }
-
-            digits[ldx] += 1;
-
-            return digits;
-            
-
+            digits[i] = 0;
         }
-        else
-        {
-            digits[ldx] = ilast;
-            return digits;
-        }
+
+        int[] newD = new int[digits.length + 1];
+        newD[0] = 1;
+        return newD;
     }
 }
